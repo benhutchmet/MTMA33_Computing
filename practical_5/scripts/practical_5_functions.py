3b3e36fa03ebd681d7f4c42cf08b718f1c8e843a
Ben Hutchins <117102154+benhutchmet@users.noreply.github.com>
2023-10-22 14:59:13 +0000

    long function

--- practical_5/scripts/practical_5_functions.py
+++ practical_5/scripts/practical_5_functions.py
@@ -3,13 +3,15 @@ import numpy as np
 import matplotlib.pyplot as plt
 import random
 
+
 def readindata(file):
     '''Opens a file named by the user and puts the data into an array
     Input:  file name (full path)
     Output:  array containing the data contained in the file'''
 
     timeseries = np.genfromtxt(file)
-    return(timeseries)
+    return (timeseries)
+
 
 def calculate_statistics(timeseries):
     '''Calculates the statistics of rainfall from a timeseries
@@ -20,7 +22,7 @@ def calculate_statistics(timeseries):
          mean rainfall on rainy days, 
          standard deviation rainfall amount on rainy days] '''
 
-    #initialising the counter variables
+    # initialising the counter variables
     count_rr = 0
     count_dr = 0
     count_dd = 0
@@ -31,7 +33,7 @@ def calculate_statistics(timeseries):
     # rain (rr), or dry day (rd).
     # We also count number of dry days followed by rain (dr) or another dry day (dd).
     ndays = len(timeseries)
-    for i in np.arange(1,ndays):
+    for i in np.arange(1, ndays):
         if ((timeseries[i-1] > 0) & (timeseries[i] > 0)):
             count_rr = count_rr + 1
 
@@ -45,7 +47,7 @@ def calculate_statistics(timeseries):
             count_rd = count_rd + 1
 
         if (timeseries[i] > 0):
-            rainydays = np.append(timeseries[i],rainydays)
+            rainydays = np.append(timeseries[i], rainydays)
 
     # Based on the counts in the previous part of the code, we calculate prr (probability of rain given rain the day before)
     # and pdr (probability of rain given no rain on the day before).
@@ -56,9 +58,10 @@ def calculate_statistics(timeseries):
     meanrain = np.mean(rainydays)
     sdrain = np.std(rainydays)
 
-    return([prr,pdr,meanrain,sdrain])
+    return ([prr, pdr, meanrain, sdrain])
 
-def gen_timeseries(stats,length_ts):
+
+def gen_timeseries(stats, length_ts):
     '''Generates a time series with user defined rainfall statistics
     Inputs:  an array of rainfall statistics in the form output by calculate_statistics 
     [probability of rain given rain the day before, 
@@ -87,14 +90,14 @@ def gen_timeseries(stats,length_ts):
 #        gen_occurrence[0] = 1
 #    else:
 #        gen_occurrence[0] = 0
-        
-    gen_intensity[0] = random.gauss(meanrain,sdrain)
 
-    for i in np.arange(1,length_ts):
+    gen_intensity[0] = random.gauss(meanrain, sdrain)
+
+    for i in np.arange(1, length_ts):
 
         # Generate the intensity for each point in time.
         # Here drawing from a Gaussian distribution of intensity.
-        gen_intensity[i] = np.abs(random.gauss(meanrain,sdrain))
+        gen_intensity[i] = np.abs(random.gauss(meanrain, sdrain))
 
         # generate the occurrence at each point in time
 
@@ -102,7 +105,7 @@ def gen_timeseries(stats,length_ts):
             # previous day was a rain day
             if random.random() < prr:
                 # set current day to rain
-                gen_occurrence[i] = 1   
+                gen_occurrence[i] = 1
             else:
                 # set current day to dry
                 gen_occurrence[i] = 0
@@ -117,9 +120,10 @@ def gen_timeseries(stats,length_ts):
 
     outts = gen_occurrence * gen_intensity
 
-    return(outts)
+    return (outts)
 
-def flood_risk_timeseries(timeseries,threshold=75,accumdays=5):
+
+def flood_risk_timeseries(timeseries, threshold=75, accumdays=5):
     '''counts the number of times a threshold is breached and outputs the 
     risk of the threshold being breached.  
     Inputs:  time series array, rainfall threshold, days of accumulation
@@ -131,20 +135,22 @@ def flood_risk_timeseries(timeseries,threshold=75,accumdays=5):
 
     # loop over the time series putting changing the 0 in the counter array to 1 if the
     # threshold is breached
-    for i in np.arange(accumdays,ndaypoints): #Looping over days in time series
-        #Testing whether cumulative rainfall is greater than the threshold
-        if (np.sum(timeseries[i-accumdays:i]) > threshold): 
+    for i in np.arange(accumdays, ndaypoints):  # Looping over days in time series
+        # Testing whether cumulative rainfall is greater than the threshold
+        if (np.sum(timeseries[i-accumdays:i]) > threshold):
             # Change the counter array element from zero to 1
-            count_floods[i] = 1 
+            count_floods[i] = 1
 
     # The number of days with flood is the sum of count_floods array.
-    # Calculating the probability that a day is in flood. 
+    # Calculating the probability that a day is in flood.
     probflood = np.sum(count_floods)/ndaypoints
 
-    return(probflood)
+    return (probflood)
 
 # Define a function to assess the flood risk using the observed
 # time series path and the flood threshold
+
+
 def assess_flood_risk(path: str, threshold: int = 75) -> float:
     """
     Function which uses the functions readindata(), calculate_statistics(),
@@ -167,6 +173,44 @@ def assess_flood_risk(path: str, threshold: int = 75) -> float:
 
     # Generate a synthetic time series using the statistics of the observed time series
     synthetic_ts = gen_timeseries(stats, len(observed_ts))
+
+    # Assess the flood risk using the synthetic time series
+    flood_risk = flood_risk_timeseries(synthetic_ts, threshold)
+
+    return flood_risk
+
+# Define a function which will assess the flood risk
+# for a very long simulated time series - e.g.  1,000,000 points
+# Using the functions readindata(), calculate_statistics(),
+# gen_timeseries() and flood_risk_timeseries()
+# Inputs to the function should be the path to the observed
+# time series, the length of the time series to be generated
+# and the flood threshold
+
+
+def assess_flood_risk_long(path: str, length: int = 1000000, threshold: int = 75) -> float:
+    """
+    Function which uses the functions readindata(), calculate_statistics(),
+    gen_timeseries() and flood_risk_timeseries() to assess the flood risk, 
+    according to statistics obtained from the observed time series.
+
+    Args:
+        path (str): Path to the observed time series file.
+        length (int, optional): Length of the synthetic time series. Defaults to 1000000.
+        threshold (int, optional): Flood threshold (mm). Defaults to 75.
+
+    Returns:
+        flood_risk (float): Probability of a flood being in progress.
+    """
+
+    # Read in the observed time series
+    observed_ts = readindata(path)
+
+    # Calculate the statistics of the observed time series
+    stats = calculate_statistics(observed_ts)
+
+    # Generate a synthetic time series using the statistics of the observed time series
+    synthetic_ts = gen_timeseries(stats, length)
 
     # Assess the flood risk using the synthetic time series
     flood_risk = flood_risk_timeseries(synthetic_ts, threshold)
